1d41c61c56b1e616ae40a403be6e0c51a71f6c67
VyHuynh <htvy@apcs.vn>
2020-07-13 23:08:08 +0700

    A01 - 2D sprite and Animation

---
+++ Assets/Script/PinkDogController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinkDogController : MonoBehaviour
+{
+    public GameObject pinkDog;
+    public GameObject heart;
+    Animator heartAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        heartAnim = heart.GetComponent<Animator>();
+    }
+    public void VisibleObj() {
+        heartAnim.GetComponent<SpriteRenderer>().enabled = true;
+    }
+    public void InvisibleObj() {
+        heartAnim.GetComponent<SpriteRenderer>().enabled = false;
+    }
+    public void SetTriggerHeart() {
+        heartAnim.SetTrigger("HeartTrigger");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SetTriggerHeart();
+        }
+    }
+}
